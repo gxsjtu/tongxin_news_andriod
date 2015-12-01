b1cccc987965b4af2530f57e303d94d5e8a54bac
acqj <acqj123>
2015-12-01 14:28:42 +0800

    update

--- app/src/main/java/com/tongxin/info/activity/userActivity.java
+++ app/src/main/java/com/tongxin/info/activity/userActivity.java
@@ -48,6 +48,7 @@ public class userActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+        mobile = UserUtils.Tel;
         initViews();
         //checkVersion();
         initData();
